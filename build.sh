cf539e93a0620c4fffe4678b72d50838a67c1e26
mike <mike@mint-VirtualBox>
2016-01-25 17:49:07 +0100

    Store maven repos in user home

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --rm -v "$PWD":/usr/src/mymaven:rw -v /tmp/docker-m2cache:/root/.m2:rw -w /usr/src/mymaven maven:3-jdk-8 mvn clean package
+sudo docker run -it --rm -v "$PWD":/usr/src/mymaven:rw -v ~/.m2:/root/.m2:rw -w /usr/src/mymaven maven:3-jdk-8 mvn clean package
